0e2760a5adfcf11e01e6746891cb27a1ce646637
tochisuke221 <tochisuke221@gmail.com>
2023-09-13 18:47:03 +0900

    モバイルとPCの両方対応

--- src/users.tsx
+++ src/users.tsx
@@ -1,4 +1,5 @@
 import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
+import { useMediaQuery, Theme } from "@mui/material";
 
 // export const UserList = () => (
 //     <List>
@@ -16,12 +17,42 @@ import { List, SimpleList, Datagrid, TextField, EmailField } from "react-admin";
 // );
 
 
-export const UserList = () => (
-  <List>
-    <SimpleList
-      primaryText={(record) => record.name}
-      secondaryText={(record) => record.username}
-      tertiaryText={(record) => record.email}
-    />
-  </List>
-);
+// export const UserList = () => (
+//   <List>
+//     <SimpleList
+//       primaryText={(record) => record.name}
+//       secondaryText={(record) => record.username}
+//       tertiaryText={(record) => record.email}
+//     />
+//   </List>
+// );
+
+// モバイルとPC
+export const UserList = () => {
+    const isSmall = useMediaQuery(theme => theme.breakpoints.down("sm"));
+
+    return (
+        <List>
+          {
+              isSmall ? (
+                <SimpleList
+                    primaryText={(record) => record.name}
+                    secondaryText={(record) => record.username}
+                    tertiaryText={(record) => record.email}
+                />
+              ) : (
+                <Datagrid rowClick="edit">
+                    <TextField source="id" />
+                    <TextField source="name" />
+                    <TextField source="username" />
+                    <EmailField source="email" />
+                    <TextField source="address.street" />
+                    <TextField source="phone" />
+                    <TextField source="website" />
+                    <TextField source="company.name" />
+                </Datagrid>
+              )
+          }
+        </List>
+    )
+}
